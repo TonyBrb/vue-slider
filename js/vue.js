c16f18b234188a3784c612ab52eab65c275f0744
TonyBrb <anthonybarbagallo95@gmail.com>
2021-11-11 16:47:58 +0100

    conversione in vue del vecchio codice

---
+++ js/vue.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+
+  el: '#app',
+
+  data:{
+    images: ['img/01.jpg','img/02.jpg','img/03.jpg','img/04.jpg','img/05.jpg'],
+    counter: 0
+  
+  },
+
+  methods:{
+    nextSlide(){
+      this.counter++;
+      if(this.counter > this.images.length - 1){
+        this.counter = 0;
+      }
+    },
+
+    prevSlide(){
+      this.counter--;
+      if(this.counter < 0){
+        this.counter = this.images.length-1;
+      }
+    }
+  }
+
+});
